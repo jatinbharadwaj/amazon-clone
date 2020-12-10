6b2e173ab0a593171e4d86a94d2e2d3d7eba6789
jatinbharadwaj <jitinrcks@gmail.com>
2020-12-11 05:16:14 +0530

    Navbar/Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, Everyone </h1>
+    <div className="app">
+      <Header />
+      {/* Header */}
+      {/* Home */}
     </div>
   );
 }
